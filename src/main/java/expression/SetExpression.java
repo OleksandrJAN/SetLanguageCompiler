e5a12e2962484f4c174df1ba19eab8b9517c473e
Александр <medvedev.sasha2000@mail.ru>
2020-05-23 22:51:49 +0300

    Fix error (if set contains variables need to return new SetValue)

--- src/main/java/expression/SetExpression.java
+++ src/main/java/expression/SetExpression.java
@@ -4,6 +4,7 @@ import domain.Element;
 import domain.Set;
 import domain.values.SetValue;
 import domain.values.Value;
+import org.apache.commons.lang3.SerializationUtils;
 import storage.Variables;
 
 import java.util.SortedSet;
@@ -21,33 +22,43 @@ public class SetExpression implements Expression {
         Set set = (Set) setValue.getValue();
         String setView = set.getView();
 
-        while (setView.contains("$")) {
-            int variablePosition = setView.indexOf('$') + 1;
-            String variableName = getVariableName(setView, variablePosition);
+        if (setView.contains("$")) {
+            // надо вернуть новое SetValue, т.к. если мы заменим переменные на текущем, то при повторном вызове этого
+            // шаблона мы получим множество со значениями переменных после 1го вызова
+            // пример: A = ${a,b}; for(2 раза) A = A + ${$A}
+            // без возвращения нового объекта: A = {a,b,{a,b}}, т.к. после замены у нас вместо мн-ва ${$A} находится мн-во {{a,b}}
+            // с возвращением нового объекта: A = {a,b,{a,b},{a,b,{a,b}}}
+            Set setWithVariables = SerializationUtils.clone(set);
+            while (setView.contains("$")) {
+                // pos of first variable name
+                int variablePosition = setView.indexOf('$') + 1;
+                String variableName = getVariableName(setView, variablePosition);
 
-            Value variableValue = Variables.get(variableName);
-            if (variableValue instanceof SetValue) {
-                Set variableElement = (Set) variableValue.getValue();
+                Value variableValue = Variables.get(variableName);
+                if (variableValue instanceof SetValue) {
+                    Set variableElement = (Set) variableValue.getValue();
 
-                SortedSet<Element> elements = set.getElements();
-                for (Element element : elements) {
-                    String elementStr = element.toString();
-                    if (elementStr.contains("$" + variableName)) {
-                        //  replace '$variableName' by variableElement
-                        String elementVariableView = variableElement.getView();
-                        String newElementWithVariable = elementStr.replace("$" + variableName, elementVariableView);
+                    SortedSet<Element> elements = setWithVariables.getElements();
+                    for (Element element : elements) {
+                        String elementStr = element.toString();
+                        if (elementStr.contains("$" + variableName)) {
+                            //  replace '$variableName' by variableElement
+                            String elementVariableView = variableElement.getView();
+                            String newElementWithVariable = elementStr.replace("$" + variableName, elementVariableView);
 
-                        //  remove old element and add new element
-                        elements.remove(element);
-                        set.addElement(new Element(newElementWithVariable));
-                        break;
+                            //  remove old element and add new element
+                            elements.remove(element);
+                            setWithVariables.addElement(new Element(newElementWithVariable));
+                            break;
+                        }
                     }
-                }
 
-                setView = set.getView();    //update new view of current set
-            } else {
-                throw new RuntimeException("Variable '" + variableName + "' in set '" + setView + "' not found");
+                    setView = setWithVariables.getView();    //update new view of current set
+                } else {
+                    throw new RuntimeException("Variable '" + variableName + "' in set '" + setView + "' not found");
+                }
             }
+            return new SetValue(setWithVariables);
         }
 
         return setValue;
